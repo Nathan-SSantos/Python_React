2b2d25c42f1f7037a98b9f23ba4640173b1023ff
Nathan-SSantos <109868156+Nathan-SSantos@users.noreply.github.com>
2023-04-27 21:26:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/postgres'
+app = Flask(_name_)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost:5432/pool'
 db = SQLAlchemy(app)
 
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
-class Aluno(db.Model):
-    id = db.Column('id', db.Integer, primary_key=True)
-    nome = db.Column('nome', db.String(50))
-    cpf = db.Column('cpf', db.String(50))
-    curso = db.Column('curso', db.String(50))
-    periodo = db.Column('periodo', db.String(50)),
+class Aluin(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(50))
+    cpf = db.Column(db.String(50), unique=True)
+    curso = db.Column(db.String(50))
+    periodo = db.Column(db.String(50))
     
-    def __init__(self, nome, cpf, curso, periodo):
-            self.nome = nome
-            self.cpf = cpf
-            self.curso = curso
-            self.periodo = periodo
-
+    def _init_(self, nome, cpf, curso, periodo):
+        self.nome = nome
+        self.cpf = cpf
+        self.curso = curso
+        self.periodo = periodo
 
 
 @app.route('/')
 def index():
-    alunos = Aluno.query.all()
-    alunos = []
+    alunos = Aluin.query.all()
+    alunos_list = []
     for aluno in alunos:
         aluno_dict = {
             'id': aluno.id,
@@ -35,24 +35,29 @@ def index():
             'curso': aluno.curso,
             'periodo': aluno.periodo
         }
-        alunos.append(aluno_dict),
-    return jsonify(alunos)
-
-@app.route('/add', methods=['POST']) 
- def add():  
-  
-     nome = request.json.get('nome') 
-     cpf = request.json.get('cpf') 
-     curso = request.json.get('curso') 
-     periodo = request.json.get('periodo') 
-
-  
-     aluno = Alunos(nome=nome, cpf=cpf, curso=curso, periodo=periodo) 
-
-     db.session.add(aluno) 
-     db.session.commit()
+        alunos_list.append(aluno_dict)
+    return jsonify(alunos_list)
 
 
+@app.route('/alunos', methods=['POST'])
+def create_aluno():
+    nome = request.json.get('nome')
+    cpf = request.json.get('cpf')
+    curso = request.json.get('curso')
+    periodo = request.json.get('periodo')
 
-if __name__ == '__main__':
+    aluno = Aluin(nome=nome, cpf=cpf, curso=curso, periodo=periodo)
+    db.session.add(aluno)
+    db.session.commit()
+
+    return jsonify({
+        'id': aluno.id,
+        'nome': aluno.nome,
+        'cpf': aluno.cpf,
+        'curso': aluno.curso,
+        'periodo': aluno.periodo
+    }), 201
+
+
+if _name_ == '_main_':
     app.run(debug=True)
